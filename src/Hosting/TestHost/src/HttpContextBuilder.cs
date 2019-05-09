cb11ec3932d8621e53bda2a6eb7012b9df199003
Andrew Stanton-Nurse <andrew@stanton-nurse.com>
2019-05-09 11:22:15 -0700

    remove culture flow

--- src/Hosting/TestHost/src/HttpContextBuilder.cs
+++ src/Hosting/TestHost/src/HttpContextBuilder.cs
@@ -96,11 +96,8 @@ namespace Microsoft.AspNetCore.TestHost
             }
             else
             {
-                // Capture the current culture and restore it inside the server execution context.
-                var currentCulture = CultureInfo.CurrentCulture;
                 ThreadPool.UnsafeQueueUserWorkItem(_ =>
                 {
-                    CultureInfo.CurrentCulture = currentCulture;
                     _ = RunRequestAsync();
                 }, null);
             }
